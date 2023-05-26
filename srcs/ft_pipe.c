faf638cd575b03b6f6fbbd85a635b355c711b782
Julien Silance <jusilanc@shi-c2-p5.s19.be>
2023-05-26 19:40:38 +0200

    fix bug de pipe

--- srcs/ft_pipe.c
+++ srcs/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:16:48 by avancoll          #+#    #+#             */
-/*   Updated: 2023/05/26 18:52:07 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:40:17 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_pipe(t_lst_cmd *cmd)
 	{
 		if (pipe(fd) < 0)
 			return (-1);
-		// printf("\033[0;34mHERE\033[0m\n");
 		cmd->fd_out = fd[0];
 		cmd->next->fd_in = fd[1];
 		cmd = cmd->next;
